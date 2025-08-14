6c6113890f72624bd4a47057e3eea0a0c3aec451
Andrei Stoica <68513477+andreicstoica@users.noreply.github.com>
2025-08-14 14:45:51 -0400

    wording tweaks

--- src/constants/intro.ts
+++ src/constants/intro.ts
@@ -1,5 +1,5 @@
-export const INTRO_TEXT = `I love reading, but am a slow reader and find listening to podcasts much easier. Text-to-speech is slowly improving, but many speed-up features aren’t great. I built this demo to my favorite podcast app's smart speed feature.`;
+export const INTRO_TEXT = `I love reading, but am a slow reader and find listening to podcasts much easier. Text-to-speech is slowly improving, but many speed-up features aren’t great. I built this demo inspired by my favorite podcast app's smart speed feature.`;
 
-export const INTRO_DETAILS = `My first time using your API, I initially tried features available only to limited customers in the v3. I switched to v2 for a simpler implementation. I really enjoyed searching your documentation by voice! I’m excited to eventually try v3 features like advanced tagging and normalization.`;
+export const INTRO_DETAILS = `My first time using your API, I initially tried features available only to limited customers in the v3, so I switched to v2. I really enjoyed searching your documentation by voice! I’m excited to eventually use features like advanced tagging and normalization.`;
 
 export const INTRO_IMPROVEMENTS = `My current approach focuses on speeding up audio by adding small delays around important breaks in prose like punctuation and paragraphs.`;
