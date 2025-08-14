745ddb374bceac60fc4f9155eb6a944bc54f6004
Andrei Stoica <68513477+andreicstoica@users.noreply.github.com>
2025-08-13 20:00:23 -0400

    vercel pls v2

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    // Allow imports like '@/components/...'
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
+};
 
 module.exports = nextConfig;
